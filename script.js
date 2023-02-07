fc3c7e76734583c0a5536eeacc69459d0893f001
Reshma <reshmashaik0910@gmail.com>
2023-02-07 10:27:25 +0000

    Updted Projects

--- script.js
+++ script.js
@@ -5,8 +5,10 @@
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
+            console.log(this)
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active"); 
+            console.log(button.dataset.id)
              })
         });
     document.querySelector(".theme-btn").addEventListener("click", () => {
